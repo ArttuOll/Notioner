186cdf74c09b16021261a6fa6298d3cf2550dee2
ArttuOll <arttuoll.tech@tutanota.com>
2021-05-21 12:29:00 +0300

    Read environment variables from .env

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from sys import stdin
+from dotenv import dotenv_values
 import json
+
+env_variables = dotenv_values(".env")
 
 json_string = stdin.read()
 json_data = json.loads(json_string)
-print("Tämä data tulee täältä!!!", json_data)
+print(env_variables)
