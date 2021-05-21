4dec6c01a9ddc2a2393608578ace5e26164914c5
ArttuOll <arttuoll.tech@tutanota.com>
2021-05-21 13:20:25 +0300

    Create a template JSON-object for Fina

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-from sys import stdin
+from sys import stdin, stderr
+from os import path
 from dotenv import dotenv_values
 import json
 
@@ -6,4 +7,12 @@ env_variables = dotenv_values(".env")
 
 json_string = stdin.read()
 json_data = json.loads(json_string)
-print(env_variables)
+
+filepath = path.join(path.dirname(__file__), "../request_body_templates/fina_request_template.json")
+try:
+    with open(filepath, "r") as body_template_file:
+        body_template_string = body_template_file.read()
+        body_template = json.loads(body_template_string)
+        print(body_template)
+except IOError as error:
+    print(f"Problem opening body template for reading: {error}", file=stderr)
