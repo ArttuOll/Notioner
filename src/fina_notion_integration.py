05f085b764f0e730c4ded1525f5e9421e8c67057
ArttuOll <arttuoll.tech@tutanota.com>
2021-05-22 15:50:41 +0300

    Add doc comments

--- src/fina_notion_integration.py
+++ src/fina_notion_integration.py
@@ -1,3 +1,6 @@
+"""Integrates fina into notion. Reads the fina-notion-template and fills its empty fields with
+appropriate data."""
+
 from notioner_utils import read_json_data_from_stdin, read_body_template_json_from_file, post_data_to_notion, get_environment_variables
 
 DATA_TYPE_INDICES_IN_FINA_JSON = {
@@ -14,13 +17,14 @@ def _send_fina_data_to_notion(template, database_id, index_in_fina_data):
 
         data["parent"]["database_id"] = database_id
         data["properties"]["Kuukausi"]["select"]["name"] = json_data[0]["month"]
-        post_data_to_notion(data)
+        # post_data_to_notion(data)
         print(data)
 
 
 json_data = read_json_data_from_stdin()
 body_template_json = read_body_template_json_from_file()
 environment_variables = get_environment_variables()
+
 _send_fina_data_to_notion(body_template_json,
                           environment_variables["INCOME_DB_ID"],
                           DATA_TYPE_INDICES_IN_FINA_JSON["income"])
--- src/notioner_utils.py
+++ src/notioner_utils.py
@@ -1,7 +1,9 @@
+"""Defines functions and procedures required by all Notion integrations."""
+
 import json
 from os import path
 from sys import stderr, stdin
-import requests
+
 from dotenv import dotenv_values
 import requests
 
@@ -9,11 +11,15 @@ ENV_VARIABLES = dotenv_values(".env")
 URL = "https://api.notion.com/v1/pages"
 
 def read_json_data_from_stdin():
+    """Reads the JSON output of another program into a dict in Notioner. This is how Notioner reads
+    data from other programs."""
     json_string = stdin.read()
     return json.loads(json_string)
 
 
 def read_body_template_json_from_file():
+    """Reads the request body template of a Notion page or database, contained in a separate
+    file."""
     filepath = path.join(path.dirname(__file__),
                          "../request_body_templates/fina_request_template.json")
     try:
@@ -26,6 +32,7 @@ def read_body_template_json_from_file():
 
 
 def post_data_to_notion(data):
+    """Makes a POST request to Notion, using the standard headers defined by the Notion API."""
     headers = {
                 "Authorization": f"Bearer {ENV_VARIABLES['API_KEY']}",
                 "Content-Type": "application/json",
@@ -38,4 +45,6 @@ def post_data_to_notion(data):
 
 
 def get_environment_variables():
+    """Returns the contents of the global .env file as dict. This is where API-keys and DB-ids are
+    stored in this program."""
     return ENV_VARIABLES
